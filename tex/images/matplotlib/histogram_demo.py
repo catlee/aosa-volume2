e362621c75aa27da4f2dc12a7779c960e20d8231
jdh <jdh@dd5aa6cc-de63-413d-92ad-03a14aac4e6a>
2011-10-20 14:28:42 +0000

    added pyplot section
    git-svn-id: http://www.aosabook.org/svn/volume2@561 dd5aa6cc-de63-413d-92ad-03a14aac4e6a

---
+++ tex/images/matplotlib/histogram_demo.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.random.randn(10000)
+plt.hist(x, 100)
+plt.title(r'Normal distribution with $\mu=0,\ \sigma=1$')
+plt.savefig('histogram_demo.pdf')
+plt.show()
