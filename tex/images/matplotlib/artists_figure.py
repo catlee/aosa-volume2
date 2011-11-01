d716b5b19affb893f715428b74ce6e61eb930e21
mdro <mdro@dd5aa6cc-de63-413d-92ad-03a14aac4e6a>
2011-11-01 17:14:07 +0000

    Change font of circled numbers
    git-svn-id: http://www.aosabook.org/svn/volume2@608 dd5aa6cc-de63-413d-92ad-03a14aac4e6a

--- tex/images/matplotlib/artists_figure.py
+++ tex/images/matplotlib/artists_figure.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import matplotlib.pyplot as plt
 from matplotlib import font_manager
 import numpy as np
@@ -9,10 +11,10 @@ plt.title('A simple plot')
 plt.xlabel('time (s)')
 plt.ylabel('voltage (mV)')
 prop = font_manager.FontProperties(size=48)
-plt.figtext(0.91, 0.9, r'$\mathcircled{1}$', fontproperties=prop)
-plt.figtext(0.81, 0.8, r'$\mathcircled{2}$', fontproperties=prop)
-plt.figtext(0.81, 0.52, r'$\mathcircled{3}$', fontproperties=prop)
-plt.figtext(0.62, 0.91, r'$\mathcircled{4}$', fontproperties=prop)
-plt.figtext(0.01, 0.3, r'$\mathcircled{5}$', fontproperties=prop)
-plt.figtext(0.58, 0.01, r'$\mathcircled{6}$', fontproperties=prop)
+plt.figtext(0.91, 0.9, ur'①', fontproperties=prop)
+plt.figtext(0.81, 0.8, ur'②', fontproperties=prop)
+plt.figtext(0.81, 0.52, ur'③', fontproperties=prop)
+plt.figtext(0.62, 0.91, ur'④', fontproperties=prop)
+plt.figtext(0.01, 0.3, ur'⑤', fontproperties=prop)
+plt.figtext(0.58, 0.01, ur'⑥', fontproperties=prop)
 plt.savefig('artists_figure.pdf')
